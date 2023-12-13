f5d8046a592306bd23aea0c197ca9b8cb707162e
Benoit SERRA <oupsman@oupsman.fr>
2023-12-13 13:50:38 +0100

    Day 13

--- d12/advent_12.go
+++ d12/advent_12.go
@@ -6,10 +6,10 @@ import (
 	"strings"
 	"strconv"
 	"fmt"
-//	"sync"
 )
 
 var cache = make(map[string]int)
+
 func readFile(fname string) []string {
 	var lines []string
 	file, err := os.Open(fname)
@@ -27,7 +27,7 @@ func readFile(fname string) []string {
 func recursiveArrangements(springs string, groups []int) int {
 	key := springs
 	for _, group := range groups {
-		key += strconv.Itoa(group)+"," 
+		key += strconv.Itoa(group) + ","
 	}
 	if v, ok := cache[key]; ok {
 		return v
@@ -45,7 +45,7 @@ func recursiveArrangements(springs string, groups []int) int {
 	}
 	if strings.HasPrefix(springs, ".") {
 		res := recursiveArrangements(strings.TrimPrefix(springs, "."), groups)
-		cache[key] = res	
+		cache[key] = res
 		return res
 	}
 
@@ -80,19 +80,18 @@ func recursiveArrangements(springs string, groups []int) int {
 	return 0
 }
 
-
 func main() {
 	INPUT := "input_12.txt"
 	// INPUT := "test_12.txt"
-	
+
 	fileContent := readFile(INPUT)
 	sum_part1 := 0
 
 	for _, line := range fileContent {
 		var comb []int
-		lineContent  := strings.Split(line, " ")
+		lineContent := strings.Split(line, " ")
 		for _, Scomb := range strings.Split(lineContent[1], ",") {
-			conv, _ :=  strconv.Atoi(Scomb)
+			conv, _ := strconv.Atoi(Scomb)
 			comb = append(comb, conv)
 		}
 		sum_part1 += recursiveArrangements(lineContent[0], comb)
@@ -100,38 +99,25 @@ func main() {
 	}
 	fmt.Println("Part1:", sum_part1)
 	sum_part2 := 0
-//	mutex := sync.RWMutex{}
-//	wg := sync.WaitGroup{}
 
 	for _, line := range fileContent {
-	//	wg.Add(1)
-		// fmt.Println("Line: ", line)
-	//	go func (line string) {
-			var comb []int
-			lineContent  := strings.Split(line, " ")
-			springs := ""
-			for i:=0; i <5; i++ {
-				springs += lineContent[0]
-				if i<4 {
-					springs += "?"
-				}
+		var comb []int
+		lineContent := strings.Split(line, " ")
+		springs := ""
+		for i := 0; i < 5; i++ {
+			springs += lineContent[0]
+			if i < 4 {
+				springs += "?"
 			}
-			for i:=0; i <5; i++ {
-				for _, Scomb := range strings.Split(lineContent[1], ",") {
-					conv, _ :=  strconv.Atoi(Scomb)
-					comb = append(comb, conv)
-				}
+		}
+		for i := 0; i < 5; i++ {
+			for _, Scomb := range strings.Split(lineContent[1], ",") {
+				conv, _ := strconv.Atoi(Scomb)
+				comb = append(comb, conv)
 			}
-//			mutex.RLock()
-//			mutex.Lock()
-			sum_part2 += recursiveArrangements(springs, comb)
-//			mutex.Unlock()
-//			mutex.RUnlock()
-//			wg.Done()
-	//	}(line)
-
+		}
+		sum_part2 += recursiveArrangements(springs, comb)
 	}
-	
-	//wg.Wait()
+
 	fmt.Println("Part2:", sum_part2)
 }
---
+++ d13/advent13.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"fmt"
+)
+
+
+func readFile(fname string) []string {
+	var lines []string
+	file, err := os.Open(fname)
+	if err != nil {
+		fmt.Println(err)
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+	return lines
+}
+
+func findHorSymetry(puzzle []string, smudge bool) int {
+	puzzleW := len(puzzle[0])
+	puzzleH := len(puzzle)
+	
+// 	for counter, _ := range puzzle {
+	for counter := 0; counter < puzzleH -1; counter++ {
+		diff := 0
+		for col := 0; col < puzzleW; col++ {
+			for offset := 0;; offset++ {
+				lowRow := counter - offset
+				highRow := counter + offset + 1
+				if lowRow < 0 || highRow >= puzzleH {
+					break;
+				}
+				if puzzle[lowRow][col] != puzzle[highRow][col] {
+					diff++
+				}
+			}
+		}
+		if smudge && diff == 1 {
+			return counter + 1
+		} else if ! smudge && diff == 0 {
+			return counter + 1
+		}
+	}
+	return 0
+}
+
+func findVertSymetry(puzzle []string, smudge bool) int {
+	columns := make([]string, len(puzzle[0]))
+	
+	for j:=0; j<len(puzzle[0]); j++ {
+		var column string
+		for i:=0; i < len(puzzle); i++  {
+			column += string(puzzle[i][j])
+		}
+		columns[j] = column
+	}
+	return findHorSymetry(columns, smudge)
+}
+
+func main() {
+	INPUT := "input_13.txt"
+	// INPUT := "test_13.txt"
+	var puzzles [][]string
+	lines := readFile(INPUT)
+	counter := 0
+	for counter < len(lines) {
+		var puzzle []string
+		
+		for lines[counter] != "" && counter < len(lines){
+			puzzle = append(puzzle, lines[counter])
+			counter++
+			if counter == len(lines) {
+				break
+			}
+		}
+		puzzles = append(puzzles, puzzle)
+		counter++
+
+	}
+
+	sum_part1 := 0
+	sum_part2 := 0
+	for _, puzzle := range puzzles {
+		sum_part1 += findVertSymetry(puzzle, false)
+		sum_part1 += findHorSymetry(puzzle, false)*100
+		sum_part2 += findVertSymetry(puzzle, true)
+		sum_part2 += findHorSymetry(puzzle, true)*100			
+		
+	}
+	fmt.Println("Part1:", sum_part1)
+	fmt.Println("Part2:", sum_part2)
+
+}
