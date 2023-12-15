fe2700ceb60a747d279fca4d9e460b9cfa649f1c
Benoit SERRA <oupsman@oupsman.fr>
2023-12-15 09:13:44 +0100

    Rework the day15 to have a single loop for both parts

--- d15/advent15.go
+++ d15/advent15.go
@@ -6,8 +6,14 @@ import (
 	"fmt"
 	"time"
 	"strings"
-	
+	"regexp"
+	"slices"
 )
+
+type Lens struct {
+	Label string
+	Length int
+}
 
 func readFile(fname string) []string {
 	var lines []string
@@ -37,6 +43,7 @@ func hash(input string) int {
 
 func main() {
 	// var cache = make(map[string]int)
+
 	timeStart := time.Now()
 	INPUT := "input_15.txt"
 	// INPUT := "test_15.txt"	
@@ -45,7 +52,34 @@ func main() {
 	for _, value := range fileContent {
 		sum_part1 += hash(value)
 	}
+
+	boxes := [256][]Lens{}
+
+	sum_part2 := 0
+
+	re := regexp.MustCompile(`(\w+)([-=])(\d*)`)
+
+	for _, input := range re.FindAllStringSubmatch(strings.Join(readFile(INPUT), "\n"), -1) {
+		box := &boxes[hash(input[1])]
+		index := slices.IndexFunc(*box, func(l Lens) bool { return l.Label == input[1] })
+
+		if input[2] == "-" && index != -1 {
+			*box = slices.Delete(*box, index, index+1)
+		} else if input[2] == "=" && index != -1 {
+			(*box)[index] = Lens{input[1], int(input[3][0] - '0')}
+		} else if input[2] == "=" {
+			*box = append(*box, Lens{input[1], int(input[3][0] - '0')})
+		}
+	}
+
+	for i, b := range boxes {
+		for j, l := range b {
+			sum_part2 += (i + 1) * (j + 1) * l.Length
+		}
+	}
+
 	fmt.Println("Part1:", sum_part1)
+	fmt.Println("Part2:", sum_part2)
 	fmt.Printf("Time: %.2fms\n", float64(time.Since(timeStart).Microseconds())/1000)
 
 }
